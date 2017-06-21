c41a8f3a6d2455ce3f3aa000fdc241d4c376aef0
CodHeK <gaganganapathyas@yahoo.in>
2017-06-21 11:27:33 +0530

    Some more additions

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 $(window).on('load', function() { // makes sure the whole site is loaded
     $("#status").fadeOut(); // will first fade out the loading animation
     $("#preloader").delay(500).fadeOut("slow"); // will fade out the white DIV that covers the website.
-    $("#navg").show(1500);
+    $("#navg").show(1200);
 });
 });
 
